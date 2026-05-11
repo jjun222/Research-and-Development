e45b9621e783993a74cf86d4eab6c86ea16b905f
jjun222 <junhyuk0110@naver.com>
2026-05-11 13:08:05 +0900

    Add NavigationService for navigation management

---
+++ CarecallAssist/Application/carecall_app/lib/services/navigation_service.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NavigationService {
+  NavigationService._();
+
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
+}
