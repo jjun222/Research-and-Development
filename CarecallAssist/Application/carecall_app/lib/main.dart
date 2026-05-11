3604eefb105d050417d307565d9c0b407cc3e7e1
jjun222 <junhyuk0110@naver.com>
2026-05-11 13:09:14 +0900

    Initialize Firebase and setup background messaging

---
+++ CarecallAssist/Application/carecall_app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+import 'app/app.dart';
+import 'firebase_options.dart';
+import 'services/fcm_service.dart';
+
+@pragma('vm:entry-point')
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  debugPrint('백그라운드 메시지 수신: ${message.messageId}');
+  debugPrint('데이터: ${message.data}');
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+  runApp(const CareCallApp());
+
+  await FcmService.instance.initialize();
+}
